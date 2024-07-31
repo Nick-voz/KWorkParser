7729eccd08a64b61ded3a7c77d24ef94fad44179
«Nick-voz» <nik.vozisow@gmail.com>
2024-07-31 17:21:45 +0300

    remove test(loger test) lines

--- app.py
+++ app.py
@@ -4,7 +4,3 @@ from logger_conf import loger_config
 
 loger_config()
 logger = logging.getLogger(__name__)
-try:
-    tmp = 1 / 0
-except ZeroDivisionError as e:
-    logger.error("do not do that", exc_info=e)
